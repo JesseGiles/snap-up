e2d670e361e8baa0225e1bb521c9629adff7152c
Jesse Giles <jesse.giles@telus.com>
2023-02-03 09:37:10 -0700

    delete some default react files/folders that are not required, ran create react app, tested some pure html/css card flip mechanics, as well as one in the react app

--- snap-up/src/App.js
+++ snap-up/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./favicon.ico";
+import "./App.css";
+
+import FlippableCard from "./components/flippable-card";
+import FlippableCard2 from "./components/flippable-card2";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <br></br>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Play Snap-up
         </a>
       </header>
+      <FlippableCard />
+      <FlippableCard2 />
     </div>
   );
 }
