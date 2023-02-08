d1489ce992f310b54b8a91ca71deb4c7c5536f71
Jesse Giles <jesse.giles@telus.com>
2023-02-08 09:32:49 -0700

    pushing server folder

--- server/server.js
+++ server/server.js
@@ -21,77 +21,32 @@ const socketIO = require("socket.io")(http, {
   },
 });
 
+let users = [];
+
 socketIO.on("connection", (socket) => {
   console.log(`⚡: ${socket.id} player just connected!`);
 
   //Listens and logs the message to the console
-  socket.on("playerdata", (data) => {
-    console.log(data);
-  });
-
-  let room = 0;
-
-  // socket.on("join", ({ name }, callback) => {
-
-  socket.on("join", ({ name }) => {
-    console.log("received name:", name);
-    console.log("room is:", room);
-    const { error, player } = addPlayer({ id: socket.id, name, room });
-
-    if (error) return callback(error);
-
-    // Emit will send message to the player
-    // who had joined
-    socket.emit("message", {
-      player: "admin",
-      text: `${player.name},
-        welcome to room ${player.room}.`,
-    });
-
-    // Broadcast will send message to everyone
-    // in the room except the joined player
-    socket.broadcast.to(player.room).emit("message", {
-      player: "admin",
-      text: `${player.name}, has joined`,
-    });
-
-    socket.join(player.room);
-
-    socketIO.to(player.room).emit("roomData", {
-      room: player.room,
-      players: getPlayersInRoom(player.room),
-    });
-    // callback();
-  });
-
-  // socket.on("sendMessage", (message, callback) => {
-  // socket.on("sendMessage", (message) => {
-  //   const player = getPlayer(socket.id);
-  //   socketIO
-  //     .to(player.room)
-  //     .emit("message", { player: player.name, text: message });
-
-  //   socketIO.to(player.room).emit("roomData", {
-  //     room: player.room,
-  //     players: getPlayersInRoom(player.room),
-  //   });
-  //   // callback();
-  // });
-
-  socket.on("disconnect", () => {
-    const player = removePlayer(socket.id);
-    if (player) {
-      socketIO.to(player.room).emit("message", {
-        player: "admin",
-        text: `${player.name} had left`,
-      });
+  socket.on("message", (data) => {
+    console.log("This was received from the CLIENT:", data);
+    // console.log("All connected users: ", data);
+    // socketIO.emit("messageResponse", data);
+    users.push(data);
+    console.log("All connected users:", users);
+    if (users.length >= 2) {
+      socketIO.emit("newUserResponse", users);
+    } else {
+      console.log("Waiting for another player to join...");
     }
   });
-});
 
-app.get("/api", (req, res) => {
-  res.json({
-    message: "Hello world",
+  socket.on("disconnect", () => {
+    console.log("🔥: A user disconnected");
+    users = users.filter((user) => user.socketID !== socket.id);
+    // console.log(users);
+    //Sends the list of users to the client
+    socketIO.emit("newUserResponse", users);
+    socket.disconnect();
   });
 });
 
