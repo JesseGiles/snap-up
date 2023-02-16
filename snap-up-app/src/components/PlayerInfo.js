d8f8071a5aa68a2b0b0ea02fff18a89b24d71d7e
Jesse Giles <jesse.giles@telus.com>
2023-02-15 19:42:05 -0700

    fixed css styling for screen proportions

--- snap-up-app/src/components/PlayerInfo.js
+++ snap-up-app/src/components/PlayerInfo.js
@@ -5,8 +5,8 @@ import Splash from "./Splash";
 import Home from "./Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import socketIO from "socket.io-client";
-const socket = socketIO.connect("https://snap-up-app.up.railway.app/");
-// const socket = socketIO.connect("http://localhost:4000");
+// const socket = socketIO.connect("https://snap-up-app.up.railway.app/");
+const socket = socketIO.connect("http://localhost:4000");
 
 function PlayerInfo(props) {
   const [playerName, setPlayerName] = useState("");
@@ -24,49 +24,48 @@ function PlayerInfo(props) {
 
   return (
     <BrowserRouter>
-      
-        <Routes>
-          <Route path="/" element={<Splash />}></Route>
-          <Route path="/home" element={<Home />}></Route>
-          <Route
-            path="/playerform"
-            element={
-              <PlayerForm
-                room={room}
-                setRoom={setRoom}
-                socket={socket}
-                setPlayerInfo={props.setPlayerInfo}
-                playerName={playerName}
-                setPlayerName={setPlayerName}
-                avatarSelected={avatarSelected}
-                setAvatarSelected={setAvatarSelected}
-                deckOneSelected={deckOneSelected}
-                setDeckOneSelected={setDeckOneSelected}
-                deckTwoSelected={deckTwoSelected}
-                setDeckTwoSelected={setDeckTwoSelected}
-                setAvatarImage={setAvatarImage}
-                setDeckOneImage={setDeckOneImage}
-                setDeckTwoImage={setDeckTwoImage}
-              />
-            }
-          ></Route>
-          <Route
-            path="/game"
-            element={
-              <Connection
-                room={room}
-                socket={socket}
-                playerName={playerName}
-                avatar={avatarSelected}
-                deckOne={deckOneSelected}
-                deckTwo={deckTwoSelected}
-                avatarImage={avatarImage}
-                deckOneImage={deckOneImage}
-                deckTwoImage={deckTwoImage}
-              />
-            }
-          ></Route>
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Splash />}></Route>
+        <Route path="/home" element={<Home />}></Route>
+        <Route
+          path="/playerform"
+          element={
+            <PlayerForm
+              room={room}
+              setRoom={setRoom}
+              socket={socket}
+              setPlayerInfo={props.setPlayerInfo}
+              playerName={playerName}
+              setPlayerName={setPlayerName}
+              avatarSelected={avatarSelected}
+              setAvatarSelected={setAvatarSelected}
+              deckOneSelected={deckOneSelected}
+              setDeckOneSelected={setDeckOneSelected}
+              deckTwoSelected={deckTwoSelected}
+              setDeckTwoSelected={setDeckTwoSelected}
+              setAvatarImage={setAvatarImage}
+              setDeckOneImage={setDeckOneImage}
+              setDeckTwoImage={setDeckTwoImage}
+            />
+          }
+        ></Route>
+        <Route
+          path="/game"
+          element={
+            <Connection
+              room={room}
+              socket={socket}
+              playerName={playerName}
+              avatar={avatarSelected}
+              deckOne={deckOneSelected}
+              deckTwo={deckTwoSelected}
+              avatarImage={avatarImage}
+              deckOneImage={deckOneImage}
+              deckTwoImage={deckTwoImage}
+            />
+          }
+        ></Route>
+      </Routes>
     </BrowserRouter>
   );
 }
